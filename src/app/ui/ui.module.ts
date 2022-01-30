6c64b83d927d85979dbcbf08add60fcc1d978089
sbdm05 <s.baibou@gmail.com>
2022-01-30 18:38:21 +0100

    ui2 agile

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiComponent } from './pages/ui/ui.component';
+import { Ui2Component } from './pages/ui2/ui2.component';
 
 
 
 
 @NgModule({
   declarations: [
-    UiComponent
+    UiComponent,
+    Ui2Component
   ],
   imports: [
     CommonModule
   ], 
   exports: [
-    UiComponent
+    UiComponent, 
+    Ui2Component
   ]
 })
 export class UiModule { }
