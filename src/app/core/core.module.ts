d6e5496763cffa32e255fa35426e933daac933d8
sbdm05 <s.baibou@gmail.com>
2022-01-30 14:50:28 +0100

    ng-content

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,18 +3,27 @@ import { CommonModule } from '@angular/common';
 import { UiModule } from '../ui/ui.module';
 import { TemplatesModule } from '../templates/templates.module';
 import { IconsModule } from '../icons/icons.module';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    NavComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
   ], 
   exports : [
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
     UiModule, 
     TemplatesModule, 
     IconsModule, 
-
   ]
 })
 export class CoreModule { }
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UiComponent } from './pages/ui/ui.component';
+
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UiComponent
+  ],
   imports: [
     CommonModule
+  ], 
+  exports: [
+    UiComponent
   ]
 })
 export class UiModule { }
