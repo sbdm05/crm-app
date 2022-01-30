08ed6fba0f1bd3bd69d074f01d72942d1f176520
sbdm05 <s.baibou@gmail.com>
2022-01-30 15:35:15 +0100

    ngClass

--- src/app/ui/pages/ui/ui.component.ts
+++ src/app/ui/pages/ui/ui.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UiComponent implements OnInit {
 
+  public open = true;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public toggle(): void{
+    this.open = !this.open
+    console.log(this.open)
+  }
+  
 }
