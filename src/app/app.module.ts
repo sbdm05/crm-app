b5c2e1f491189dce86ebfb97f3d2791178434126
sbdm05 <s.baibou@gmail.com>
2022-01-30 17:37:07 +0100

    etape 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-
 @NgModule({
   declarations: [
     AppComponent
--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IconNavComponent } from './icon-nav/icon-nav.component';
+import { IconCloseComponent } from './icon-close/icon-close.component';
+import { IconEditComponent } from './icon-edit/icon-edit.component';
+import { IconDeleteComponent } from './icon-delete/icon-delete.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    IconNavComponent,
+    IconCloseComponent,
+    IconEditComponent,
+    IconDeleteComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule, 
+    FontAwesomeModule
+  ],
+  exports: [
+    IconNavComponent,
+    IconCloseComponent,
+    IconEditComponent,
+    IconDeleteComponent
   ]
 })
 export class IconsModule { }
