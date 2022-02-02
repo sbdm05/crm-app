939459403b71ed5d26ed14060ecac01c82028340
sbdm05 <s.baibou@gmail.com>
2022-02-02 10:07:11 +0100

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageSignInComponent } from './login/pages/page-sign-in/page-sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' , redirectTo:'/sign-in', pathMatch: 'full' }, 
+  {path:'sign-in', component : PageSignInComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
